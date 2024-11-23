fa58f29881d74b4d22c653d8476502b3f99aa790
sem201 <sem201@naver.com>
2024-11-23 22:53:56 +0900

    [PCCE 기충]6번 가채점

---
+++ [PCCE 기출문제] 6번=가채점.py
@@ -0,0 +1,16 @@
+numbers = [3,4]
+our_score=[85,93]
+score_list = [85, 92, 38, 93, 48, 85, 92, 56]
+
+
+def solution(numbers, our_score, score_list):
+    answer = []
+    for i in range(len(numbers)):
+        if our_score[i] == score_list[numbers[i-1]]:
+            answer.append("Same")
+        else:
+            answer.append("Different")
+    
+    return answer
+
+print(solution(numbers, our_score ,score_list))
